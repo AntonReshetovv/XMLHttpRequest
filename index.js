7e9b5d59b0381e1708b109513968a93698197a06
Anton <Reshetov190991@mail.ru>
2021-04-06 23:13:59 +0700

    Создал POST запрос, и добавил body в функцию sendRequest

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const requestURL = 'https://jsonplaceholder.typicode.com/users'
 
-function sendRequest(method, url) {
+function sendRequest(method, url, body = null) {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest()
 
         xhr.open(method, url)
         xhr.responseType = 'json' // распарсил ответ с сервера
+        xhr.setRequestHeader('Content-Type','application/json') // Сказал своим запросам что я отправляю JSON 
 
         // при помощи xhr.onload можно обработать данные, которые приходят с сервера
         xhr.onload = () => {
@@ -19,11 +20,20 @@ function sendRequest(method, url) {
         xhr.onerror = () => { // происходит, только когда запрос совсем не получилось выполнить
             reject(xhr.response)
         }
-        xhr.send()
+        xhr.send(JSON.stringify(body))
     })
 }
 
-sendRequest('GET', requestURL)
+// sendRequest('GET', requestURL)
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error))
+
+const body = {
+    name: 'Test',
+    age: 30
+}
+
+sendRequest('POST', requestURL, body)
     .then(data => console.log(data))
     .catch(error => console.log(error))
 
