bcf940ce24b8588bd0cf6060132e306975dbb790
Anton <Reshetov190991@mail.ru>
2021-04-06 22:54:07 +0700

    Добавил описание к действиям

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('work app')
+const requestURL = 'https://jsonplaceholder.typicode.com/users'
+
+const xhr = new XMLHttpRequest()
+
+xhr.open('GET', requestURL)
+
+xhr.responseType = 'json' // распарсил ответ с сервера
+
+// при помощи xhr.onload можно обработать данные, которые приходят с сервера
+xhr.onload = () => {
+    if(xhr.status >= 400){
+        console.error(xhr.response)  // Добавил проверку, чтобы обрабатывать потенциальные ошибки
+    }else {
+        console.log(xhr.response)
+    }
+}
+
+xhr.onerror = () => { // происходит, только когда запрос совсем не получилось выполнить
+    console.log(xhr.response)
+}
+    
+
+xhr.send()
+
